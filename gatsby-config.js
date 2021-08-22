4cff4da55a81a682209aeb8f5fcd8ca2c2b24442
NeonNature <mustachemaung@gmail.com>
2021-08-22 20:35:46 +0630

    chore: add features with plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Gatsby Tuto",
-  },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+    siteMetadata: {
+        title: "My First Gatsby Site",
+    },
+    plugins: [
+        "gatsby-plugin-gatsby-cloud",
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+    ],
 };
